b96215e14493ab56c709c69102b8aed53ad71a7d
momnpa333 <momnpa333@gmail.com>
2025-05-01 17:08:15 +0900

    feat: userModel.Info에 birthDate 추가

--- src/main/java/supernova/whokie/user/service/dto/UserModel.java
+++ src/main/java/supernova/whokie/user/service/dto/UserModel.java
@@ -1,5 +1,6 @@
 package supernova.whokie.user.service.dto;
 
+import java.time.LocalDate;
 import lombok.Builder;
 import supernova.whokie.user.Gender;
 import supernova.whokie.user.Role;
@@ -11,76 +12,80 @@ public class UserModel {
 
     @Builder
     public record PickedInfo(
-            Long userId,
-            String name,
-            String imageUrl
+        Long userId,
+        String name,
+        String imageUrl
     ) {
+
         public static PickedInfo from(Users user, String imageUrl) {
             return PickedInfo.builder()
-                    .userId(user.getId())
-                    .name(user.getName())
-                    .imageUrl(imageUrl)
-                    .build();
+                .userId(user.getId())
+                .name(user.getName())
+                .imageUrl(imageUrl)
+                .build();
         }
 
     }
 
     @Builder
     public record Info(
-            Long id,
-            String name,
-            String imageUrl,
-            String email,
-            Gender gender,
-            int point,
-            int age,
-            Role role,
-            Long kakaoId,
-            LocalDateTime createdAt,
-            LocalDateTime modifiedAt
+        Long id,
+        String name,
+        String imageUrl,
+        String email,
+        Gender gender,
+        int point,
+        int age,
+        Role role,
+        Long kakaoId,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt,
+        LocalDate birthDate
     ) {
 
         public static UserModel.Info from(Users user) {
             return UserModel.Info.builder()
-                    .id(user.getId())
-                    .name(user.getName())
-                    .imageUrl(user.getImageUrl())
-                    .email(user.getEmail())
-                    .gender(user.getGender())
-                    .point(user.getPoint())
-                    .age(user.getAge())
-                    .role(user.getRole())
-                    .kakaoId(user.getKakaoId())
-                    .createdAt(user.getCreatedAt())
-                    .modifiedAt(user.getModifiedAt())
-                    .build();
+                .id(user.getId())
+                .name(user.getName())
+                .imageUrl(user.getImageUrl())
+                .email(user.getEmail())
+                .gender(user.getGender())
+                .point(user.getPoint())
+                .age(user.getAge())
+                .role(user.getRole())
+                .kakaoId(user.getKakaoId())
+                .createdAt(user.getCreatedAt())
+                .modifiedAt(user.getModifiedAt())
+                .birthDate(user.getBirthDate())
+                .build();
         }
     }
 
     @Builder
     public record Point(
-            int amount
+        int amount
     ) {
 
         public static UserModel.Point from(Users user) {
             return UserModel.Point.builder()
-                    .amount(user.getPoint())
-                    .build();
+                .amount(user.getPoint())
+                .build();
         }
     }
 
     @Builder
     public record Login(
-            String jwt,
-            Long userId,
-            Role role
+        String jwt,
+        Long userId,
+        Role role
     ) {
+
         public static UserModel.Login from(String jwt, Long userId, Role role) {
             return Login.builder()
-                    .jwt(jwt)
-                    .userId(userId)
-                    .role(role)
-                    .build();
+                .jwt(jwt)
+                .userId(userId)
+                .role(role)
+                .build();
         }
     }
 }
